7b4c302b458161fe82358ae8473dd9519216276f
AurelBichop <linux.aurelien@gmail.com>
2025-05-01 23:00:58 +0200

    MapQuery Parameters and Request Payload

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -10,6 +10,7 @@ use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\DependencyInjection\Attribute\AutowireIterator;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
@@ -22,14 +23,19 @@ class VinylController extends AbstractController
         private iterable $commands
     )
     {
+        /* Exemple
         foreach ($this->commands as $command) {
             dump($command);
         }
+        */
     }
 
     #[Route('/', name: 'app_homepage')]
-    public function homepage(): Response
+    public function homepage(
+        #[MapQueryParameter] string $query = ''
+    ): Response
     {
+        dump($query);
         $tracks = [
             ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
             ['song' => 'Waterfalls', 'artist' => 'TLC'],
